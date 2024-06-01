478d961abbd07804667eb4fb6ca8abd2e15deeed
kareem Ahmed <129338520+karim306@users.noreply.github.com>
2024-06-01 18:01:44 +0300

    Update store.py

--- store.py
+++ store.py
@@ -1,27 +1,18 @@
-from flask import Flask , request , render_template  , flash  ,redirect , url_for , session 
+from flask import Flask, request, render_template, flash, redirect, url_for, session
 import sqlite3
 import re
 import bcrypt
 import validators
-from flask_limiter import Limiter 
-from flask_limiter.util import get_remote_address
-#___________________________________________________________________________________
+from flask_limiter import Limiter, get_remote_address
 
-#___________________________________________________________________________________
-#     SALTING       
 def hash_password(password):
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode(), salt)
-
     return hashed_password.decode()
 
 def is_password_match(entered_password, stored_hash):
-    stored_hash_bytes = stored_hash.encode()
+    return bcrypt.checkpw(entered_password.encode(), stored_hash.encode())
 
-    return bcrypt.checkpw(entered_password.encode(), stored_hash_bytes)
-
-#____________________________________________________________________________
-#     ---------- STRONG   PASSWORD ---------------
 def is_strong_password(password):
     min_length = 8
     require_uppercase = True
@@ -32,26 +23,17 @@ def is_strong_password(password):
     if len(password) < min_length:
         return False
 
-    if require_uppercase and not any(char.isupper() for char in password):
-        return False
-
-    if require_lowercase and not any(char.islower() for char in password):
-        return False
-
-    if require_digit and not any(char.isdigit() for char in password):
-        return False
-
-    if require_special_char and not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+    if (require_uppercase and not any(char.isupper() for char in password)) or \
+       (require_lowercase and not any(char.islower() for char in password)) or \
+       (require_digit and not any(char.isdigit() for char in password)) or \
+       (require_special_char and not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password)):
         return False
 
     return True
-#___________________________________________________________________________________
 
 app = Flask(__name__)
 app.secret_key = "SUPER-key"
-limiter = Limiter(app=app, key_func=get_remote_address ,default_limits=["10 per minute"])
-
-#================USERS-TABLE===============================================
+limiter = Limiter(app=app, key_func=get_remote_address, default_limits=["10 per minute"])
 
 def init_db():
     connection = sqlite3.connect("database.db")
@@ -68,30 +50,27 @@ def init_db():
 
     connection.commit()
 
-
 def add_user(username, password):
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
     hashed_password = hash_password(password)
-    query =  '''INSERT INTO users (username , password)  VALUES(? , ?)'''
-    cursor.execute(query , (username , hashed_password) ) 
+    query = '''INSERT INTO users (username , password)  VALUES(?, ?)'''
+    cursor.execute(query , (username, hashed_password) )
     connection.commit()
 
-def get_user(username ):
+def get_user(username):
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
-    query = '''SELECT * FROM USERS WHERE USERNAME = ? '''
-    cursor.execute(query , (username, ))
+    query = 'SELECT * FROM USERS WHERE USERNAME = ?'
+    cursor.execute(query, (username,))
     return cursor.fetchone()
 
-def get_user_by_username(username ):
-    connection = sqlite3.connect("database.db") 
+def get_user_by_username(username):
+    connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
-    query = f'''SELECT * FROM USERS WHERE USERNAME = '{username}' '''
+    query = f'SELECT * FROM USERS WHERE USERNAME = "{username}"'
     cursor.execute(query)
     return cursor.fetchone()
-
-
 
 def get_all_users():
     cursor = connection.cursor()
@@ -99,119 +78,104 @@ def get_all_users():
     cursor.execute(query)
     return cursor.fetchall()
 
-
-
-#=======INIT THE MOVIES TABLE==============================================
 def init_movies_table():
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
 
-
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS movies (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                user_id INTEGER NOT NULL,
-                title TEXT NOT NULL ,
-                descreption TEXT ,
-                price REAL NOT NULL,
-                image_url TEXT,
-                is_sold BOOLEAN DEFAULT 0,
-                FOREIGN KEY (user_id) REFERENCES users (id)
-                )
-              ''')
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            title TEXT NOT NULL,
+            descreption TEXT,
+            price REAL NOT NULL,
+            image_url TEXT,
+            is_sold BOOLEAN DEFAULT 0,
+            FOREIGN KEY (user_id) REFERENCES users (id)
+        )
+    ''')
     connection.commit()
 
-def add_movie(user_id ,  title , descreption,price , image_url=None ):
+def add_movie(user_id, title, descreption, price, image_url=None):
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
-    query = ''' INSERT INTO movies (user_id , title ,descreption , price , image_url) VALUES (?,?,?,?,?)'''
-    cursor.execute(query , (user_id , title ,descreption , price , image_url))
+    query = '''INSERT INTO movies (user_id, title, descreption, price, image_url) VALUES (?, ?, ?, ?, ?)'''
+    cursor.execute(query, (user_id, title, descreption, price, image_url))
     connection.commit()
 
 def get_movie(movie_id):
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
-    query = '''SELECT * FROM movies WHERE id = ?'''
-    cursor.execute(query , (movie_id , ))
+    query = 'SELECT * FROM movies WHERE id = ?'
+    cursor.execute(query, (movie_id,))
     return cursor.fetchone()
 
-def get_user_movie(user_id  ):
+def get_user_movie(user_id):
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
-    query = '''SELECT * FROM movies WHERE user_id = ?'''
-    cursor.execute(query, (user_id , ))
+    query = 'SELECT * FROM movies WHERE user_id = ?'
+    cursor.execute(query, (user_id,))
     return cursor.fetchone()
 
 def get_all_movies():
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
-    query = '''SELECT * FROM movies'''
+    query = 'SELECT * FROM movies'
     cursor.execute(query)
     return cursor.fetchall()
 
-def is_movie_sold( movie_id):
+def is_movie_sold(movie_id):
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
-    query = '''SELECT is_sold FROM movies WHERE id = ?'''
+    query = 'SELECT is_sold FROM movies WHERE id = ?'
     cursor.execute(query, (movie_id,))
     return cursor.fetchone()[0]
 
-def mark_movie_as_sold( movie_id):
+def mark_movie_as_sold(movie_id):
     connection = sqlite3.connect("database.db")
-
     cursor = connection.cursor()
-
-    # Retrieve movie information
-    movie_query = '''SELECT price, user_id FROM movies WHERE id = ?'''
+    movie_query = 'SELECT price, user_id FROM movies WHERE id = ?'
     cursor.execute(movie_query, (movie_id,))
     movie_data = cursor.fetchone()
 
     if movie_data:
         movie_price, user_id = movie_data
-
-        # Update movie as sold
-        update_query = '''UPDATE movies SET is_sold = 1 WHERE id = ?'''
+        update_query = 'UPDATE movies SET is_sold = 1 WHERE id = ?'
         cursor.execute(update_query, (movie_id,))
         connection.commit()
-
-        # Update owner's balance
-        update_balance_query = '''UPDATE users SET balance = balance + ? WHERE id = ?'''
+        update_balance_query = 'UPDATE users SET balance = balance + ? WHERE id = ?'
         cursor.execute(update_balance_query, (movie_price, user_id))
         connection.commit()
 
-
-
-#=======INIT comment section==============================================
 def init_comments_table():
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
 
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS comments (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                user_id INTEGER NOT NULL,
-                movie_id INTEGER NOT NULL,
-                text TEXT NOT NULL ,
-                timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                FOREIGN KEY (movie_id) REFERENCES movies (id),
-                FOREIGN KEY (user_id) REFERENCES users (id)
-
-                )
-              ''')
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            movie_id INTEGER NOT NULL,
+            text TEXT NOT NULL,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (movie_id) REFERENCES movies (id),
+            FOREIGN KEY (user_id) REFERENCES users (id)
+        )
+    ''')
     connection.commit()
 
-def add_comment(movie_id ,user_id   ,text):
+def add_comment(movie_id, user_id, text):
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
-    query = ''' INSERT INTO comments (movie_id ,user_id   ,text) VALUES (?,?,?)'''
-    cursor.execute(query , (movie_id ,user_id   ,text ,))
+    query = '''INSERT INTO comments (movie_id, user_id, text) VALUES (?, ?, ?)'''
+    cursor.execute(query, (movie_id, user_id, text))
     connection.commit()
 
-def get_comments_for_movie( movie_id):
+def get_comments_for_movie(movie_id):
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
     query = '''
-        SELECT  users.username, comments.text, comments.timestamp
+        SELECT users.username, comments.text, comments.timestamp
         FROM comments
         JOIN users ON comments.user_id = users.id
         WHERE comments.movie_id = ?
@@ -219,17 +183,12 @@ def get_comments_for_movie( movie_id):
     cursor.execute(query, (movie_id,))
     return cursor.fetchall()
 
-
-
-#============================Routing======================================
-
 @app.route('/')
 def index():
     if 'username' in session:
         if session['username'] == 'admin':
             return list(get_all_users())
-        else:
-            return render_template("index.html", movies=get_all_movies())
+        return render_template("index.html", movies=get_all_movies())
     return "You are not logged in."
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -239,126 +198,110 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        user = get_user( username )
+        user = get_user(username)
 
         if user:
-            if is_password_match(password ,user[2]):
-               session['username'] = user[1]
-               session['user_id']  = user[0]
-               return redirect(url_for('uploadMovie'))
+            if is_password_match(password, user[2]):
+                session['username'] = user[1]
+                session['user_id'] = user[0]
+                return redirect(url_for('uploadMovie'))
             else:
-                flash("Wrong Cardinals", "danger")
+                flash("Wrong password", "danger")
                 return render_template("login.html")
-
         else:
-            flash("Wrong Cardinals", "danger")
+            flash("User not found", "danger")
             return render_template("login.html")
 
     return render_template("login.html")
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    #post handl 
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-    #HANDLING THE PASSWORD 
-        if not is_strong_password(password) :
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if not is_strong_password(password):
             flash("Please choose a strong password", "danger")
             return render_template("register.html")
-    #HANDLE
-        is_user_found = get_user(username)
-        if is_user_found:
-            flash("user is already created ", "danger")
+
+        if get_user(username):
+            flash("User already exists", "danger")
             return render_template("register.html")
+
         add_user(username, password)
         return redirect(url_for("login"))
-    #get handl 
-    else :
-        return render_template("register.html")
 
+    return render_template("register.html")
 
 @app.route('/uploadMovie', methods=['GET', 'POST'])
 @limiter.limit('10 per minute')
 def uploadMovie():
-    #post handl 
-    if not 'user_id' in session:
-        flash("Please login , first" , "danger")
+    if 'user_id' not in session:
+        flash("Please login first", "danger")
         return redirect(url_for("login"))
 
     if request.method == "POST":
         movieImage = request.files['image']
-        if movieImage.filename =='':
-            flash("Image is required" , "danger")
-            return render_template("upload-movie.html")
-
-        if not (validators.allowed_file(movieImage.filename))or not validators.allowed_file_size(movieImage):
-            flash("IInvalid file is uploaded " , "danger")
-            return render_template("upload-movie.html")
-
-     
-
-
         title = request.form['title']
         descreption = request.form['description']
         price = request.form['price']
-        movieImage = request.files['image']
-        image_url = f"uploads/{movieImage.filename}"      
+
+        if not movieImage:
+            flash("Image is required", "danger")
+            return render_template("upload-movie.html")
+
+        if not validators.allowed_file(movieImage.filename) or not validators.allowed_file_size(movieImage):
+            flash("Invalid file is uploaded", "danger")
+            return render_template("upload-movie.html")
+
+        image_url = f"uploads/{movieImage.filename}"
         movieImage.save("static/" + image_url)
-        user_id = session['user_id']
-        add_movie( user_id, title, descreption ,price , image_url)
-        
+        add_movie(session['user_id'], title, descreption, price, image_url)
         return redirect(url_for('index'))
 
     return render_template("upload-movie.html")
 
-
-@app.route('/movie/<movie_id>',methods=['GET','POST'])
+@app.route('/movie/<movie_id>')
 def getMovie(movie_id):
-    # Retrieve movie information and comments from the database
-    movie = get_movie( movie_id)
-    comments = get_comments_for_movie( movie_id)
+    movie = get_movie(movie_id)
+    if not movie:
+        return "Movie not found", 404
 
+    comments = get_comments_for_movie(movie_id)
     return render_template('movie.html', movie=movie, comments=comments)
 
-
-@app.route('/add-comment/<movie_id>', methods=['POST'])
+@app.route('/add_comment/<movie_id>', methods=['POST'])
 def addComment(movie_id):
     text = request.form['comment']
-    user_id = session['user_id']
-    add_comment( movie_id, user_id, text)
+    if not text:
+        return "Comment is required", 400
+
+    if 'user_id' not in session:
+        return "Please login first", 401
+
+    movie_id = int(movie_id)
+    add_comment(movie_id, session['user_id'], text)
     return redirect(url_for("getMovie", movie_id=movie_id))
 
-
-
-
-@app.route('/buy-Movie/<movie_id>',methods=['POST'])
+@app.route('/buy_movie/<movie_id>', methods=['POST'])
 def buy_movie(movie_id):
-    Movie = get_movie( movie_id)
-    # check if the Movie is already sold 
-    is_sold = is_movie_sold(movie_id)
-    if is_sold == 0:
-       if Movie:
-            mark_movie_as_sold( movie_id)
-            flash(f"Congratulations You have bought the movie!","success")
-            return redirect(url_for("getMovie", movie_id=movie_id))
-       else:
-            return redirect(url_for("getMovie", movie_id=movie_id))
-    else:
-        flash("Sorry the  is already sold", "danger")
-        return redirect(url_for('getMovie', movie_id=movie_id))   
+    if 'user_id' not in session:
+        return "Please login first", 401
 
+    movie = get_movie(movie_id)
+    if not movie or movie.is_sold:
+        return "Movie not found or already sold", 404
+
+    mark_movie_as_sold(movie_id)
+    flash("Movie purchased successfully!", "success")
+    return redirect(url_for("getMovie", movie_id=movie_id))
 
 @app.route("/hello")
 def hello():
     name = request.args.get("name")
-    return "hello" 
+    return f"hello, {name}" if name else "hello"
 
-#=========================MAIN==============================================
 if __name__ == "__main__":
     init_db()
     init_movies_table()
     init_comments_table()
-   
-   
     app.run(debug=True)
